572a343eab51dea27199cd28e7361f953beedc54
Joyce Chen <joyce.chen@samsara.com>
2022-12-10 15:11:03 -0800

    day10, part 1

--- day-X.ts
+++ day-X.ts
@@ -2,144 +2,47 @@
 
 import { testInput, realInput } from "./input";
 
-interface Position {
-    rowIndex: number;
-    colIndex: number;
-}
-
-// Takes input of "R 4\nU 4..", and returns discrete one-distance moves.
-function getHeadMoves(inputStr: string): string[] {
-    const result: string[] = [];
-
-    const lines = inputStr.split("\n");
-    lines.forEach(elem => {
-        const pieces = elem.split(" ");
-        const numSteps = parseInt(pieces[1]);
-        for (let i = 0; i < numSteps; i++) {
-            result.push(pieces[0]);
-        }
-    })
-
-    return result;
-}
-
-// return new Tail Position
-function getTailPosition(newHeadPosition: Position, tailPosition: Position): Position {
-    // Same node => OK, no move necessary
-    if (newHeadPosition.rowIndex === tailPosition.rowIndex && newHeadPosition.colIndex === tailPosition.colIndex) {
-        return tailPosition;
-    }
-
-    // Adjacent => OK, no move necessary
-    const rowDiff = Math.abs(newHeadPosition.rowIndex - tailPosition.rowIndex);
-    const colDiff = Math.abs(newHeadPosition.colIndex - tailPosition.colIndex);
-    if (rowDiff <= 1 && colDiff <= 1) {
-        return tailPosition
-    }
-
-    // Otherwise, need to move Tail to 'catch up'
-    if (rowDiff === 0) {
-        if (newHeadPosition.colIndex > tailPosition.colIndex) {
-            return {
-                rowIndex: tailPosition.rowIndex,
-                colIndex: tailPosition.colIndex + 1,
-            }
-        } else {
-            return {
-                rowIndex: tailPosition.rowIndex,
-                colIndex: tailPosition.colIndex - 1,
-            }
-        }
-    } else if (colDiff === 0) {
-        if (newHeadPosition.rowIndex > tailPosition.rowIndex) {
-            return {
-                rowIndex: tailPosition.rowIndex + 1,
-                colIndex: tailPosition.colIndex,
-            }
-        } else {
-            return {
-                rowIndex: tailPosition.rowIndex - 1,
-                colIndex: tailPosition.colIndex,
-            }
-        }
-    } else { // 'not touching, and aren't in same row or column' => tail always moves 1 step diagonally to keep up
-        // which direction (NW, NE, SE, SW) to go?
-        if (newHeadPosition.rowIndex < tailPosition.rowIndex) {
-            if (newHeadPosition.colIndex < tailPosition.colIndex) {
-                return { // NW
-                    rowIndex: tailPosition.rowIndex - 1,
-                    colIndex: tailPosition.colIndex - 1,
-                }
-            } else {
-                return { // NE
-                    rowIndex: tailPosition.rowIndex - 1,
-                    colIndex: tailPosition.colIndex + 1,
-                }
-            }
-        } else {
-            if (newHeadPosition.colIndex < tailPosition.colIndex) {
-                return { // SW
-                    rowIndex: tailPosition.rowIndex + 1,
-                    colIndex: tailPosition.colIndex - 1,
-                }
-            }
-            else {
-                return { // SE
-                    rowIndex: tailPosition.rowIndex + 1,
-                    colIndex: tailPosition.colIndex + 1,
-                }
-            }
-        }
-    }
-}
 
 function runInput(str: string) {
-    const allHeadMoves = getHeadMoves(str);
+    const commands = str.split("\n");
 
-    const tailVisitedLocations = new Set<string>();
+    const cycleNumToValue = new Map<number, number>();
 
-    // Start at {1000, 1000} to account for potential changes in direction
-    let headPosition: Position = { rowIndex: 1000, colIndex: 1000 };
-    const tailPositions = new Map<number, Position>();
-    for (let i = 1; i <= 9; i++) {
-        tailPositions.set(i, {rowIndex: 1000, colIndex: 1000 });
+    let currentCycle = 1;
+    let currentRegisterValue = 1;
+    let addOperationToFinish: number | undefined;
+    commands.forEach(command => {
+        if (addOperationToFinish) {
+            currentRegisterValue += addOperationToFinish;
+            addOperationToFinish = undefined;
+        }
+
+
+        if (command === "noop") {
+            cycleNumToValue.set(currentCycle, currentRegisterValue);
+            currentCycle ++;
+        } else {
+            cycleNumToValue.set(currentCycle, currentRegisterValue);
+            cycleNumToValue.set(currentCycle+1, currentRegisterValue);
+            currentCycle += 2; 
+
+            addOperationToFinish = parseInt(command.split(" ")[1]);
+        }
+    })
+    // Do we need to finish the last operation?
+
+    for (let i = 1; i <= cycleNumToValue.size; i++) {
+        console.log(i, "---", cycleNumToValue.get(i))
     }
 
-    tailVisitedLocations.add("1000-1000");
-
-    allHeadMoves.forEach(move => {
-        console.log("----")
-        // console.log("head", headPosition, "tail", tailPosition);
-        console.log(move);
-
-        // Calculate new Head Position
-        if (move === "R") {
-            headPosition.colIndex += 1;
-        } else if (move === "L") {
-            headPosition.colIndex -= 1;
-        } else if (move === "U") {
-            headPosition.rowIndex -= 1;
-        } else { // D
-            headPosition.rowIndex += 1;
-        }
-    
-        const newTail1Position = getTailPosition(headPosition, tailPositions.get(1));
-        tailPositions.set(1, newTail1Position);
-
-        for (let i = 2; i <= 9; i++) {
-            const newTailXPosition = getTailPosition(tailPositions.get(i-1), tailPositions.get(i));
-            tailPositions.set(i, newTailXPosition);
-        }
-
-
-        tailVisitedLocations.add(`${tailPositions.get(9).rowIndex}-${tailPositions.get(9).colIndex}`)
+    let total = 0;
+    for (let i = 20; i <= 220; i += 40) {
+        total += i * cycleNumToValue.get(i);
+    }
 
 
 
-    });
-
-    console.log(tailVisitedLocations.size);
-
+    console.log(total)
 }
 
 
--- outDir/day-X.js
+++ outDir/day-X.js
@@ -2,128 +2,37 @@
 // tsc && node ./outDir/day-X.js
 Object.defineProperty(exports, "__esModule", { value: true });
 const input_1 = require("./input");
-// Takes input of "R 4\nU 4..", and returns discrete one-distance moves.
-function getHeadMoves(inputStr) {
-    const result = [];
-    const lines = inputStr.split("\n");
-    lines.forEach(elem => {
-        const pieces = elem.split(" ");
-        const numSteps = parseInt(pieces[1]);
-        for (let i = 0; i < numSteps; i++) {
-            result.push(pieces[0]);
-        }
-    });
-    return result;
-}
-// return new Tail Position
-function getTailPosition(newHeadPosition, tailPosition) {
-    // Same node => OK, no move necessary
-    if (newHeadPosition.rowIndex === tailPosition.rowIndex && newHeadPosition.colIndex === tailPosition.colIndex) {
-        return tailPosition;
-    }
-    // Adjacent => OK, no move necessary
-    const rowDiff = Math.abs(newHeadPosition.rowIndex - tailPosition.rowIndex);
-    const colDiff = Math.abs(newHeadPosition.colIndex - tailPosition.colIndex);
-    if (rowDiff <= 1 && colDiff <= 1) {
-        return tailPosition;
-    }
-    // Otherwise, need to move Tail to 'catch up'
-    if (rowDiff === 0) {
-        if (newHeadPosition.colIndex > tailPosition.colIndex) {
-            return {
-                rowIndex: tailPosition.rowIndex,
-                colIndex: tailPosition.colIndex + 1,
-            };
-        }
-        else {
-            return {
-                rowIndex: tailPosition.rowIndex,
-                colIndex: tailPosition.colIndex - 1,
-            };
-        }
-    }
-    else if (colDiff === 0) {
-        if (newHeadPosition.rowIndex > tailPosition.rowIndex) {
-            return {
-                rowIndex: tailPosition.rowIndex + 1,
-                colIndex: tailPosition.colIndex,
-            };
-        }
-        else {
-            return {
-                rowIndex: tailPosition.rowIndex - 1,
-                colIndex: tailPosition.colIndex,
-            };
-        }
-    }
-    else { // 'not touching, and aren't in same row or column' => tail always moves 1 step diagonally to keep up
-        // which direction (NW, NE, SE, SW) to go?
-        if (newHeadPosition.rowIndex < tailPosition.rowIndex) {
-            if (newHeadPosition.colIndex < tailPosition.colIndex) {
-                return {
-                    rowIndex: tailPosition.rowIndex - 1,
-                    colIndex: tailPosition.colIndex - 1,
-                };
-            }
-            else {
-                return {
-                    rowIndex: tailPosition.rowIndex - 1,
-                    colIndex: tailPosition.colIndex + 1,
-                };
-            }
-        }
-        else {
-            if (newHeadPosition.colIndex < tailPosition.colIndex) {
-                return {
-                    rowIndex: tailPosition.rowIndex + 1,
-                    colIndex: tailPosition.colIndex - 1,
-                };
-            }
-            else {
-                return {
-                    rowIndex: tailPosition.rowIndex + 1,
-                    colIndex: tailPosition.colIndex + 1,
-                };
-            }
-        }
-    }
-}
 function runInput(str) {
-    const allHeadMoves = getHeadMoves(str);
-    const tailVisitedLocations = new Set();
-    // Start at {1000, 1000} to account for potential changes in direction
-    let headPosition = { rowIndex: 1000, colIndex: 1000 };
-    const tailPositions = new Map();
-    for (let i = 1; i <= 9; i++) {
-        tailPositions.set(i, { rowIndex: 1000, colIndex: 1000 });
-    }
-    tailVisitedLocations.add("1000-1000");
-    allHeadMoves.forEach(move => {
-        console.log("----");
-        // console.log("head", headPosition, "tail", tailPosition);
-        console.log(move);
-        // Calculate new Head Position
-        if (move === "R") {
-            headPosition.colIndex += 1;
+    const commands = str.split("\n");
+    const cycleNumToValue = new Map();
+    let currentCycle = 1;
+    let currentRegisterValue = 1;
+    let addOperationToFinish;
+    commands.forEach(command => {
+        if (addOperationToFinish) {
+            currentRegisterValue += addOperationToFinish;
+            addOperationToFinish = undefined;
         }
-        else if (move === "L") {
-            headPosition.colIndex -= 1;
+        if (command === "noop") {
+            cycleNumToValue.set(currentCycle, currentRegisterValue);
+            currentCycle++;
         }
-        else if (move === "U") {
-            headPosition.rowIndex -= 1;
+        else {
+            cycleNumToValue.set(currentCycle, currentRegisterValue);
+            cycleNumToValue.set(currentCycle + 1, currentRegisterValue);
+            currentCycle += 2;
+            addOperationToFinish = parseInt(command.split(" ")[1]);
         }
-        else { // D
-            headPosition.rowIndex += 1;
-        }
-        const newTail1Position = getTailPosition(headPosition, tailPositions.get(1));
-        tailPositions.set(1, newTail1Position);
-        for (let i = 2; i <= 9; i++) {
-            const newTailXPosition = getTailPosition(tailPositions.get(i - 1), tailPositions.get(i));
-            tailPositions.set(i, newTailXPosition);
-        }
-        tailVisitedLocations.add(`${tailPositions.get(9).rowIndex}-${tailPositions.get(9).colIndex}`);
     });
-    console.log(tailVisitedLocations.size);
+    // Do we need to finish the last operation?
+    for (let i = 1; i <= cycleNumToValue.size; i++) {
+        console.log(i, "---", cycleNumToValue.get(i));
+    }
+    let total = 0;
+    for (let i = 20; i <= 220; i += 40) {
+        total += i * cycleNumToValue.get(i);
+    }
+    console.log(total);
 }
 /** ----- MODIFY TO CHANGE TEST VS. REAL ----- */
 // runInput(testInput);
